bc22466911da43aeeba4fba466d55de1b5a68ba6
Yann BILISSOR <algo.crypto@gmail.com>
2017-12-28 02:38:12 +0100

    add settings file with azure key

---
+++ src/csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using csharp.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace csharp
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration { get; set; }
+        static void Main(string[] args)
+        {
+
+            configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+
+            var host = configuration["host"];
+            var connstring = configuration["connstring"];
+            var endpoint = configuration["endpoint"];
+
+            var sensor = new VirtualSensor(Guid.NewGuid().ToString());
+            var hub = new AzureHubConsumer(host, endpoint, connstring);
+            var cancellationToken = new CancellationToken();
+            sensor.AddConsumer(e => hub.Consume(e).Wait());
+            Task.Factory.StartNew(sensor.Start, cancellationToken).Wait();
+        }
+    }
+}
